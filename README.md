ed65c02ed01df21ccd9397a3fe8b4e6a82e7df69
Qumeric <qumidium@gmail.com>
2016-11-08 23:32:57 +0300

    Basic file structure

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # instabattle_server
+
+## Links
+* Front-end: [repository][1]
+
+
+[1]: https://github.com/StoriesClass/instabattle
+
---
+++ manage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import os
+
+from app import create_app, db
+from flask_script import Manager, Shell
+#from app.models import User, Battle, Vote, Entry, Permission
+#from flask_migrate import Migrate, MigrateCommand
+
+app = create_app(os.environ.get('FLASK_CONFIG'))
+manager = Manager(app)
+#migrate = Migrate(app, db)
+
+
+def make_shell_context():
+    """
+    Create shell context for convenient debugging
+    """
+    return dict()
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
+
+@manager.command
+def test(name=None):
+    """
+    Run tests
+    FIXME
+    """
+    pass
+
+
+@manager.command
+def example():
+    """
+    Example command
+    FIXME
+    """
+    pass
+
+
+if __name__ == '__main__':
+    manager.run()

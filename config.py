02b1915b3b3d22270263f099546712d60a07dba7
glebwin <glebwin@gmail.com>
2016-11-15 21:51:34 +0300

    PEP8))))

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SSL_DISABLE = True # FIXME
+    SSL_DISABLE = True  # FIXME
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False # FIXME
-    SQLALCHEMY_RECORD_QUERIES = True # FIXME
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # FIXME
+    SQLALCHEMY_RECORD_QUERIES = True  # FIXME
 
     @staticmethod
     def init_app(app):
@@ -17,8 +17,8 @@ class Config:
 class TestingConfig(Config):
     WTF_CSRF_ENABLED = False  # FIXME
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URI') or \
-                              'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URI') or\
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
 
 
 config = {

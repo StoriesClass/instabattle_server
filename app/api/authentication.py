3eb6a2c3c4d8c2a9507ab7c5c9149b6b6a67d8d6
Qumeric <qumidium@gmail.com>
2016-12-25 20:51:43 +0300

    Get token by username instead of email

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -6,17 +6,16 @@ auth = HTTPBasicAuth()
 
 
 @auth.verify_password
-def verify_password(email_or_token, password):
+def verify_password(username_or_token, password):
     from app.models import User, AnonymousUser
-    print(email_or_token)
-    if email_or_token == '':
+    if username_or_token == '':
         g.current_user = AnonymousUser()
         return True
     if password == '':
-        g.current_user = User.verify_auth_token(email_or_token)
+        g.current_user = User.verify_auth_token(username_or_token)
         g.token_used = True
         return g.current_user is not None
-    user = User.query.filter_by(email=email_or_token).first()
+    user = User.query.filter_by(username=username_or_token).first()
     if not user:
         return False
     g.current_user = user
